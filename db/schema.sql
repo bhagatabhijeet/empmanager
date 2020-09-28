cbaeac13c7d3f88743247c5723e48a2fe229c9c5
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-09-27 23:53:40 -0700

    add schema and seed sql files

---
+++ db/schema.sql
@@ -0,0 +1,45 @@
+-- Create Database
+CREATE DATABASE  IF NOT EXISTS `empdb` ;
+
+USE `empdb`;
+DROP TABLE IF EXISTS `employee`;
+DROP TABLE IF EXISTS `role`;
+DROP TABLE IF EXISTS `department`;
+
+-- Table structure for table `department`
+
+CREATE TABLE `department` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+); 
+
+-- Table structure for table `role`
+
+CREATE TABLE `role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(30) DEFAULT NULL,
+  `salary` decimal(8,0) DEFAULT NULL,
+  `department_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_dept_idx` (`department_id`),
+  CONSTRAINT `fk_dept` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- Table structure for table `employee`
+
+CREATE TABLE `employee` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `role_id` int DEFAULT NULL,
+  `manager_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `manager_id_idx` (`manager_id`),
+  KEY `role_id_idx` (`role_id`),
+  CONSTRAINT `fk_manager` FOREIGN KEY (`manager_id`) REFERENCES `employee` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+
+
---
+++ db/seed.sql
@@ -0,0 +1,60 @@
+-- Seed data for database named 'empdb'
+-- @author Abhijeet Bhagat
+
+
+-- Seed data for table `department`
+
+LOCK TABLES `department` WRITE;
+
+INSERT INTO `department` (name) 
+VALUES 
+('HR') /* department id 1*/,
+('Engineering') /* department id 2*/,
+('R&D') /* department id 3*/,
+('Marketing') /* department id 4*/,
+('Accounting and Finance') /* department id 5*/,
+('Administration') /* department id 6*/,
+('Infosec') /* department id 7*/;
+
+UNLOCK TABLES;
+
+
+-- Seed data for table `role`
+
+LOCK TABLES `role` WRITE;
+
+INSERT INTO `role` (title,salary,department_id) 
+VALUES
+    ('HR Officer',100000,1), /* 1 */
+    ('HR Manager',120000,1), /* 2 */
+    ('Recruiter',90000,1), /* 3 */
+    ('Software Engineer',120000,2), /* 4 */
+    ('Senoir Software Engineer',140000,2), /* 5 */
+    ('Junior Scientist',130000,3), /* 6 */
+    ('Senior Scientist',150000,3), /* 7 */
+    ('Marketing Executive',100000,4), /* 8 */
+    ('Marketing Manager',130000,4), /* 9 */
+    ('Accounts Officer',110000,5), /* 10 */
+    ('Finance Manager',130000,5), /* 11 */
+    ('Admin Clerk',90000,6), /* 12 */
+    ('Admin Manager',100000,6), /* 13 */
+    ('Infosec Officer',90000,7), /* 14 */
+    ('Infosec Manager',120000,7); /* 15 */
+
+UNLOCK TABLES;
+
+
+-- Seed data for table `employee`
+
+LOCK TABLES `employee` WRITE;
+
+INSERT INTO `employee` (first_name,last_name,role_id,manager_id)
+VALUES 
+    ('Joeseph','Cutler',2,NULL),
+    ('Edward','Hawk',1,1),
+    ('Saina','Chopra',3,1),
+    ('Sean', 'Gomez',5,NULL),
+    ('David ', 'Sener',4,4);
+
+UNLOCK TABLES;
+
