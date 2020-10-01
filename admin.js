8cbab12854b49fa98a02bcd7f47df1327adacabb
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-09-30 17:23:42 -0700

    add documentation comments

--- admin.js
+++ admin.js
@@ -1,5 +1,9 @@
 const adminORM = require('./orm/adminorm');
 
+// Admin Entity
+// The Admin is used for seeding the data
+// Admin object was created to avoid directly using 'database.js'
+// Instead now Admin routes to adminorm which in turn calls 'database.js'
 const Admin = {
     async seedData() {
         await adminORM.seedData();
