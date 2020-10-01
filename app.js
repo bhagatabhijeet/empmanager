984857f8f83d579c55c8fff5383ed7370dcbeeb2
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-09-30 17:14:32 -0700

    add documentation comments

--- app.js
+++ app.js
@@ -31,57 +31,76 @@ console.log();
 //#endregion
 // ***********************************************************************************************************
 
+// Event handler when process exits
 process.on('exit', (code) => {
     console.log(`Thanks for using ${chalk.magentaBright('Emp.Manager!')}`);
 })
 
+// Event handler befor process exit
 process.on('beforeExit', (c) => {
     console.log(`${chalk.magentaBright('Ok!')}`);
 });
 
+/**
+ * @description init is the main function.The job of init is to route the calls to appropriate entities
+ * e.g Employee.ViewAllEmployees, Role.viewAllRoles etc.
+ * init supports 19 operations.
+ * @author Abhijeet Bhagat
+ */
 async function init() {
     let continueAsking = true;
     var ui = new inquirer.ui.BottomBar();
 
+    // Loop to ask user if he wants to continue
     while (continueAsking) {
+        // Use of bottom bar. This will show a constant message on screen
         ui.updateBottomBar(`${chalk.bgMagenta('[Enter Number To Select Operation Or Select Using Arrow Keys]')}\n`);
         let empoperation = await inquirer.prompt([
             {
                 message: 'What would you like to do? >>',
                 name: 'operation',
                 type: "rawlist",
-                pageSize: 16,
+                pageSize: 16,  // Render 15 Items at once
                 choices: [
+                    // **************   THIS IS OUR MAIN MENU *************************
+                    // Use of separator to enhance UX
                     new inquirer.Separator('---------- EMPLOYEE OPERATIONS ----------'),
-                    'View All Employees',
-                    'View All Employees In A Department',
-                    'View All Employees By Manager',
-                    'Add Employee',
-                    'Remove Employee',
-                    'Update Employee',
+                    'View All Employees',                           /* Operation 1 */
+                    'View All Employees In A Department',           /* Operation 2 */
+                    'View All Employees By Manager',                /* Operation 3 */
+                    'Add Employee',                                 /* Operation 4 */
+                    'Remove Employee',                              /* Operation 5 */
+                    'Update Employee',                              /* Operation 6 */
+                    // Use of separator to enhance UX
                     new inquirer.Separator('---------- ROLE OPERATIONS --------------'),
-                    'View All Roles',
-                    'View All Roles For A Department',
-                    'Add New Role',
-                    'Update Role',
-                    'Delete Role',
+                    'View All Roles',                               /* Operation 7 */
+                    'View All Roles For A Department',              /* Operation 8 */
+                    'Add New Role',                                 /* Operation 9 */
+                    'Update Role',                                  /* Operation 10 */
+                    'Delete Role',                                  /* Operation 11 */
+                    // Use of separator to enhance UX
                     new inquirer.Separator('---------- DEPARTMENT OPERATIONS --------'),
-                    'View All Departments',
-                    'Add New Department',
-                    'Update Department',
-                    'Delete Department',
-                    'View Total Utilized Budget Of Department',
-                    'View Total Utilized Budget By Department',
+                    'View All Departments',                         /* Operation 12 */
+                    'Add New Department',                           /* Operation 13 */
+                    'Update Department',                            /* Operation 14 */
+                    'Delete Department',                            /* Operation 15 */
+                    'View Total Utilized Budget Of Department',     /* Operation 16 */
+                    'View Total Utilized Budget By Department',     /* Operation 17 */
+                    // Use of separator to enhance UX
                     new inquirer.Separator('---------- DB ADMIN OPERATIONS ----------'),
-                    'DB Seed Data',
+                    'DB Seed Data',                                 /* Operation 18 */
                     new inquirer.Separator(),
-                    'Exit'
+                    // Allow User to gracefully exit
+                    'Exit'                                          /* Operation 19 */
                 ]
             }
         ]);
 
         // Two blank lines on the console.Just for better looking.
         console.log(); console.log();
+
+        // Switch statement to route call to approprite entities vis Employee, Department or Role
+        // All calls use await syntax
 
         switch (empoperation.operation.toUpperCase()) {
             case 'VIEW ALL EMPLOYEES':
@@ -150,7 +169,7 @@ async function init() {
                 }
                 break;
             case 'VIEW TOTAL UTILIZED BUDGET OF DEPARTMENT':
-                if(await Employee.viewTotalUtilizedBudgetOfDepartment()  === 'MAIN_MENU') {
+                if (await Employee.viewTotalUtilizedBudgetOfDepartment() === 'MAIN_MENU') {
                     continue;
                 }
                 break;
@@ -164,6 +183,7 @@ async function init() {
                 process.exit(0);
         }
 
+        // Confirmation to continue
         let continueQuestion = await inquirer.prompt([
             {
                 message: chalk.bgMagenta('Continue? : '),
@@ -175,6 +195,5 @@ async function init() {
     }
     process.exit(0);
 }
-
 
 init();
